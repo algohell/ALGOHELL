b0b33676af668569c1201b7a165563ecc89917ff
Lee Seungwoo <36434219+starhn87@users.noreply.github.com>
2019-04-21 17:35:39 +0900

    solved 226

--- 226/lsw.java
+++ 226/lsw.java
@@ -18,7 +18,7 @@ class Solution {
             TreeNode temp = root.left;
             root.left = root.right;
             root.right = temp;
-            invert(root.left);
+            invert(root.left); 
             invert(root.right);
         }
     }

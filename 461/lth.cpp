7e4868aa414e29725d8c5a19045e99e28767ceb2
Godsenal <tmqps78@gmail.com>
2019-05-31 22:32:17 +0900

    #7 solve 48, 399, 461

---
+++ 461/lth.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+  int hammingDistance(int x, int y)
+  {
+    int ans = 0;
+    while (x > 0 || y > 0)
+    {
+      ans += (x % 2) ^ (y % 2);
+      x /= 2;
+      y /= 2;
+    }
+    return ans;
+  }
+};

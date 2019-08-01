927deb3d1cb26afb53a40b58d59fc108fda92d26
haeun bae <baehaeun96@gmail.com>
2019-08-01 17:33:28 +0900

    #13 solve 239

---
+++ 239/bhe.py
@@ -0,0 +1,12 @@
+yclass Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        now = []
+        if len(nums) != 0:
+            for i in range(len(nums)-k+1):
+                now.append(max(nums[i:i+k]))
+            return now
+        elif k >= len(nums) and len(nums) != 0:
+            now.append(max(nums))
+            return now
+        elif len(nums)==0 or k==0:
+            return []

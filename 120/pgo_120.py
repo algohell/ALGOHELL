c94ff4710da0189b9886f5be78003adf55f612c2
pulum03 <pulum03@gmail.com>
2019-09-12 18:24:39 +0900

    ADD pgo 120, 75

---
+++ 120/pgo_120.py
@@ -0,0 +1,14 @@
+# 120.triangle
+#triangle = [[2], [3,4], [6,5,7], [4,1,8,3]]
+triangle = [[-1], [2,3], [1,-1,-3]]
+t = triangle
+#print(t)
+tl = len(t)
+dp = t[-1]
+print(dp)
+
+for i in range(tl-2, -1, -1):
+    for j in range(i+1):
+        dp[i] = min(dp[j], dp[j+1]) + t[i][j]
+
+print(dp[0])
---
+++ 75/pgo_75.py
@@ -0,0 +1,43 @@
+# 75. Sort Colors
+from collections import Counter
+nums=[2,0,2,1,1,0]
+
+# quick sort 이용
+# def quick_sort(nums):
+#     #print(nums)
+#     if len(nums) <= 1:
+#         print(nums)
+#     pivot = nums[len(nums) // 2]
+#     #print(pivot)
+#     lesser_nums, equal_nums, greater_nums = [], [], []
+
+#     for n in nums:
+#         if n < pivot:
+#             lesser_nums.append(n)
+#             #print('lesser_nums=', lesser_nums)
+#         elif n > pivot:
+#             greater_nums.append(n)
+#             #print('greater_nums=', greater_nums)
+#         else:
+#             equal_nums.append(n)
+#             #print('equal_nums=', equal_nums)
+    
+#     print(lesser_nums+equal_nums+greater_nums)
+    
+# quick_sort(nums)
+
+count = Counter(nums)
+print(count)
+
+for i in range(len(nums)):
+    
+    if i < count[0]:
+        nums[i] = 0
+    
+    elif i < count[0] + count[1]:
+        nums[i] = 1
+    
+    else:
+        nums[i] = 2
+        
+print(nums)

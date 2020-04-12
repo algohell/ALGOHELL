bc19b8add970bd978a1ef2403b0e651f299c8b56
Godsenal <tmqps78@gmail.com>
2020-04-12 22:37:44 +0900

    :wrench: github api OAUTH 토큰 추가

--- src/fetcher.js
+++ src/fetcher.js
@@ -2,10 +2,16 @@ const axios = require("axios");
 
 const githubApi = axios.create({
   baseURL: "https://api.github.com",
+  headers: {
+    Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+  },
 });
 
 const slackApi = axios.create({
   baseURL: "https://slack.com/api",
+  headers: {
+    Authorization: `Bearer ${process.env.SLACK_AUTH_TOKEN}`,
+  },
 });
 
 module.exports = {
